2437ce85f9b2babc337dff9c76353865080c8342
Sean <sean@codekoalas.com>
2017-12-29 14:12:40 -0600

    Fixed ip formatter

--- app.js
+++ app.js
@@ -3,6 +3,14 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 1337;
 
+const formatIp = req => {
+  if (req.headers['x-forwarded-for']) {
+    return req.headers['x-forwarded-for'].split(',')[0];
+  } else {
+    return req.ip;
+  }
+};
+
 const formatLang = lang => lang.split(',')[0];
 
 const formatSoftware = software => software.split(')')[0].split('(')[1];
@@ -13,7 +21,7 @@ app.get('/', (req, res) => {
 
 app.get('/whoami', (req, res) => {
   res.json({
-    ip: req.ip,
+    ip: formatIp(req),
     language: formatLang(req.headers['accept-language']),
     software: formatSoftware(req.headers['user-agent']),
   });
